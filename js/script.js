07fe7586aad6166f892e20c5d17d65e5ac78b4a7
maicol <maicol.matteoli@gmail.com>
2021-06-10 15:20:56 +0200

    add : caricamento secondo vettore

--- js/script.js
+++ js/script.js
@@ -15,3 +15,23 @@ do{
 }
 while(max >= vet.length || max < 0);
 console.log("il valore max è corretto");
+
+var x;
+if(min > max){ //scambio di valore 
+    x = min;
+    min = max; 
+    max = x;
+}
+
+
+vet2 = nVet(vet,min,max);
+// console.log(min);
+// console.log(max);
+console.log(vet2);
+function nVet(array,minimo,massimo){
+    var k = [];
+    for(var i=minimo;i<=massimo;i++){
+        k.push(array[i]);
+    }
+    return k;
+}
